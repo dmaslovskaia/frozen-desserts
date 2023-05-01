7d92d89734c8002df5bcde1f8db608c45f97d1fd
Daria Maslovskaya <57175646+daria-maslovskaya@users.noreply.github.com>
2023-05-01 11:59:10 -0700

    fix docker entrypoint to run db migrations on start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # If running the rails server then create or migrate existing database
-if [ "${*}" == "./bin/rails server" ]; then
+if [ "${*}" == "./bin/rails server -p 80 -b 0.0.0.0" ]; then
   ./bin/rails db:prepare
 fi
 
