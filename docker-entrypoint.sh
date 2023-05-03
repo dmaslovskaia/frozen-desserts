d208a9a3bb86eee5dd68c5d4e507e8650bf9454f
Daria Maslovskaya <57175646+daria-maslovskaya@users.noreply.github.com>
2023-05-02 18:03:26 -0700

    return fix for spec tests

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$RAILS_ENV" == "test" ] || [ "$RAILS_ENV" == "development" ]; then
   # if start in dev or test mode migrate database and run rspec tests
   bundle exec rails db:prepare
-  bundle exec rspec --format documentation --format json --fail-fast
+  bundle exec rspec --format documentation --fail-fast
 else
   # if start in prod mode only migrate database
   bundle exec rails db:prepare
